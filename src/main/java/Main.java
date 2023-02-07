d77e0faf336814f5da59ab6b4c18afd83d092432
mr87mohammadi <mahdimohammadizegan@gmail.com>
2023-02-07 18:09:00 +0330

    bugs removed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,7 @@
-package main.java;
-
 import java.util.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.io.*;
-import main.java.stemmer.*;
 
 public class Main{
 
@@ -12,22 +9,26 @@ public class Main{
     {
         Scanner scanner = new Scanner(System.in);
         String pathToFile;
-        System.out.println("Enter full directory path:");   //Default directory C:\\Users\\mahdi.mohammadi\\FullTextSearch\\Test\\
+        System.out.println("Enter full directory path:");   //Default directory C:\\Users\\mahdi.mohammadi\\Test\\
         pathToFile = scanner.nextLine();
         File folder = new File(pathToFile);
         File[] listOfFiles = folder.listFiles();
         HashMap<String, HashMap<Integer, Integer>> dataSet = new HashMap<>();
+        if(listOfFiles == null){
+            System.out.println("Given directory is empty!!");
+            System.exit(0);
+        }
         makeDataSet(pathToFile, listOfFiles, dataSet);
         System.out.println("Enter input word:");
         String key = scanner.nextLine();
         key = key.toUpperCase();
         String[] keys = key.split(" ");
         ArrayList<Integer> R1 = new ArrayList<>();
-        boolean flag1 = handlePlus(dataSet, keys, true, R1);
+        boolean isPlusEverUsed = handlePlus(dataSet, keys, R1);
         ArrayList<Integer> R2 = new ArrayList<>();
-        handleBare(dataSet, keys, R2);
+        boolean isEverUsed = handleBare(dataSet, keys, R2);
         ArrayList<Integer> keysToDelete = handleMinus(dataSet, keys);
-        ArrayList<Integer> result = combineResults(flag1, R1, R2);
+        ArrayList<Integer> result = combineResults(isPlusEverUsed, isEverUsed, R1, R2, listOfFiles.length);
         deleteKeys(keysToDelete, result);
         printResult(result);
     }
@@ -52,17 +53,18 @@ public class Main{
         return lines;
     }
 
-    private static Boolean handlePlus(HashMap<String, HashMap<Integer, Integer>> dataSet, String[] keys, Boolean flag1, ArrayList<Integer> R1) {
-        for (int i = 0; i < keys.length; i++) {
-            if (keys[i].startsWith("+")) {
+    private static Boolean handlePlus(HashMap<String, HashMap<Integer, Integer>> dataSet, String[] keys, ArrayList<Integer> R1) {
+        boolean flag1 = true;
+        for (String key : keys) {
+            if (key.startsWith("+")) {
                 flag1 = false;
-                HashMap<Integer, Integer> temp = dataSet.get(keys[i].replaceFirst("\\+", ""));
-                if(temp == null)
-                    System.out.println("key does not exist!");
+                HashMap<Integer, Integer> temp = dataSet.get(key.replaceFirst("\\+", ""));
+                if (temp == null)
+                    continue;
                 traverseHashMap(R1, temp);
             }
         }
-        return flag1;
+        return !flag1;
     }
 
     private static void traverseHashMap(ArrayList<Integer> R1, HashMap<Integer, Integer> temp) {
@@ -74,85 +76,96 @@ public class Main{
     }
 
     private static boolean handleBare(HashMap<String, HashMap<Integer, Integer>> dataSet, String[] keys, ArrayList<Integer> R2) {
+        boolean isEverUsed = false;
         boolean flag2 = true;
-        for (int i = 0; i < keys.length; i++) {
-            if((!(keys[i].startsWith("+")))&&(!(keys[i].startsWith("-")))){
-                HashMap<Integer, Integer> temp = dataSet.get(keys[i]);
-                if(temp == null)
+        for (String key : keys) {
+            if ((!(key.startsWith("+"))) && (!(key.startsWith("-")))) {
+                isEverUsed = true;
+                HashMap<Integer, Integer> temp = dataSet.get(key);
+                if (temp == null){
                     System.out.println("key does not exist!");
-                else{
-                if(flag2){
+                    System.exit(0);}
+                else {
+                    if (flag2) {
                         traverseHashMap(R2, temp);
                         flag2 = false;
+                    } else {
+                        for (int j = 0; j < R2.size(); j++) {
+                            if (!temp.containsKey(R2.get(j)))
+                                R2.remove(j);
+                        }
+                    }
                 }
-                else{
-                    for (int j = 0; j < R2.size(); j++) {
-                        if(!temp.containsKey(R2.get(j)))
-                            R2.remove(j);
-                    }}}
             }
         }
-        return flag2;
+        return isEverUsed;
     }
 
     private static ArrayList<Integer> handleMinus(HashMap<String, HashMap<Integer, Integer>> dataSet, String[] keys) {
         ArrayList<Integer> keysToDelete = new ArrayList<>();
-        for (int i = 0; i < keys.length; i++) {
-            if(keys[i].startsWith("-")){
-                HashMap<Integer, Integer> temp = dataSet.get(keys[i].replaceFirst("\\-",""));
-                if(temp == null)
-                    System.out.println("key does not exist!");
+        for (String key : keys) {
+            if (key.startsWith("-")) {
+                HashMap<Integer, Integer> temp = dataSet.get(key.replaceFirst("-", ""));
+                if (temp == null)
+                    continue;
                 traverseHashMap(keysToDelete, temp);
-            }}
+            }
+        }
         return keysToDelete;
     }
 
-    private static ArrayList<Integer> combineResults(boolean flag, ArrayList<Integer> R1, ArrayList<Integer> R2) {
+    private static ArrayList<Integer> combineResults(boolean isPlusEverUsed,boolean isEverUsed, ArrayList<Integer> R1, ArrayList<Integer> R2, int size) {
         ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < R2.size(); i++) {
-            if(R1.contains(R2.get(i))||flag)
-                result.add(R2.get(i));
+        if((!isEverUsed)&&(!isPlusEverUsed))
+                addAllToResults(result, size);
+        else if(isEverUsed&&!isPlusEverUsed)
+            result.addAll(R2);
+        else if(!isEverUsed)
+            result.addAll(R1);
+        else
+        for (Integer integer : R2) {
+            if (R1.contains(integer))
+                result.add(integer);
         }
         return result;
     }
 
     private static void printResult(ArrayList<Integer> R2) {
-        for (int i = 0; i < R2.size(); i++) {
-            System.out.println(R2.get(i));
+        for (Integer integer : R2) {
+            System.out.println(integer);
         }
     }
 
     private static void deleteKeys(ArrayList<Integer> keysToDelete, ArrayList<Integer> result) {
-        for (int i = 0; i < keysToDelete.size(); i++) {
-            if(result.contains(keysToDelete.get(i)))
-                result.remove(keysToDelete.get(i));
+        for (Integer integer : keysToDelete) {
+            result.remove(integer);
         }
     }
 
     private static void makeDataSet(String pathToFile, File[] listOfFiles, HashMap<String, HashMap<Integer, Integer>> dataSet) {
         for (int i = 0; i < listOfFiles.length; i++) {
             List<String> tempList = readFileInList(pathToFile + listOfFiles[i].getName());
-            String[] temp=tempList.get(0).split(" ");
-            PorterStemmer porterStemmer = new PorterStemmer();
-            stem(temp, porterStemmer);
-            toUpper(temp);
-            for (int j = 0; j < temp.length; j++) {
-                if(dataSet.containsKey(temp[j])){
-                    if(dataSet.get(temp[j]).containsKey(i))
-                        dataSet.get(temp[j]).put(i, dataSet.get(temp[j]).get(i)+1);
-                    else
-                        dataSet.get(temp[j]).put(i,1);
-                }
-                else{
-                    HashMap<Integer, Integer> temporary = new HashMap<>();
-                    temporary.put(i,1);
-                    dataSet.put(temp[j], temporary);
+            if (!tempList.isEmpty()){
+                String[] temp = tempList.get(0).split(" ");
+                main.java.stemmer.PorterStemmer porterStemmer = new main.java.stemmer.PorterStemmer();
+                stem(temp, porterStemmer);
+                toUpper(temp);
+                for (String s : temp) {
+                    if (dataSet.containsKey(s)) {
+                        if (dataSet.get(s).containsKey(i))
+                            dataSet.get(s).put(i, dataSet.get(s).get(i) + 1);
+                        else
+                            dataSet.get(s).put(i, 1);
+                    } else {
+                        HashMap<Integer, Integer> temporary = new HashMap<>();
+                        temporary.put(i, 1);
+                        dataSet.put(s, temporary);
+                    }
                 }
             }
         }
     }
-
-    private static void stem(String[] temp, PorterStemmer porterStemmer) {
+    private static void stem(String[] temp, main.java.stemmer.PorterStemmer porterStemmer) {
         for (int j = 0; j < temp.length; j++) {
             temp[j] = porterStemmer.stem(temp[j]);
         }
@@ -163,5 +176,9 @@ public class Main{
             temp[j] = temp[j].toUpperCase();
         }
     }
-
+    private static void addAllToResults(ArrayList<Integer> results, int size){
+        for (int i = 0; i < size; i++) {
+            results.add(i);
+        }
+    }
 }
