d50be5cd7cbba9b93b42554d2072c6d98469be11
mr87mohammadi <mahdimohammadizegan@gmail.com>
2023-03-05 09:53:13 +0330

    "Tests corrected"

--- src/SampleLibrary/SampleLibrary.Test/FileHandlerTest.cs
+++ src/SampleLibrary/SampleLibrary.Test/FileHandlerTest.cs
@@ -1,23 +1,34 @@
-﻿namespace SampleLibrary.Test;
+﻿using System;
+using System.IO;
+
+namespace SampleLibrary.Test;
 
 public class FileHandlerTest
 {
     [Theory]
-    [InlineData(".//TestFolder", ".//TestFolder\\.txt", ".//TestFolder\\a.txt")]
+    [InlineData(".//TestFolder", ".txt", "a.txt")]
     public void GetFileName_Test(string pathToFile, string firstFileName, string secondFileName)
     {
+
+        var str1 = pathToFile+$"{Path.DirectorySeparatorChar}"+firstFileName;
+        var str2 = pathToFile+$"{Path.DirectorySeparatorChar}"+secondFileName;
         var fileHandler = new FileHandler();
         fileHandler.LoadFile(pathToFile);
 
-        Assert.Equal(firstFileName, fileHandler.GetFileName(0));
-        Assert.Equal(secondFileName, fileHandler.GetFileName(1));
+        Assert.Equal(str1, fileHandler.GetFileName(0));
+        Assert.Equal(str2, fileHandler.GetFileName(1));
 
     }
 
     [Theory]
-    [InlineData(".//TestFolder", new string[] { ".//TestFolder\\.txt", ".//TestFolder\\a.txt" })]
+    [InlineData(".//TestFolder", new string[] { ".txt", "a.txt" })]
     public void LoadFiles_GetFileList_Test(string pathToFile, string[] listOfFiles)
     {
+        for (var i=0; i<listOfFiles.Length; i++)
+        {
+            listOfFiles[i] = pathToFile + $"{Path.DirectorySeparatorChar}" + listOfFiles[i];
+        }
+        
         var fileHandler = new FileHandler();
         fileHandler.LoadFile(pathToFile);
         
